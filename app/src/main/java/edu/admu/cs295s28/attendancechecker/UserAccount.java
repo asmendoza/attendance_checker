ba99c094403dcedf9392535304beb4ef867e6a3f
lvtamayo <lp.tamayo@yahoo.com>
2018-07-10 21:30:39 +0800

    Generate QRCode.

--- app/src/main/java/edu/admu/cs295s28/attendancechecker/UserAccount.java
+++ app/src/main/java/edu/admu/cs295s28/attendancechecker/UserAccount.java
@@ -1,7 +1,9 @@
 package edu.admu.cs295s28.attendancechecker;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.widget.Button;
 
 import org.androidannotations.annotations.Click;
@@ -14,10 +16,24 @@ public class UserAccount extends AppCompatActivity {
     @ViewById(R.id.button)
     Button login;
 
+    @Click(R.id.button)
+    public void generateQR(){
+        GenerateQR_.intent(this).start();
+    }
     @Click(R.id.button2)
     public void summary(){
         AttendanceSummary_.intent(this).start();
     }
+
+
+   /* public void readQr(View view){
+        Intent intent=new Intent(MainActivity.this,QrRead.class);
+        startActivity(intent);
+    }
+    public void generateQr(View view){
+        Intent intent=new Intent(this,GenerateQR.class);
+        startActivity(intent);
+    }*/
     /*@Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
