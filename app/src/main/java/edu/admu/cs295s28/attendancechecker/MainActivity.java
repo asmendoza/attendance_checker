c2bf4df8e4494d3b93d50524725022450c064c52
lvtamayo <lp.tamayo@yahoo.com>
2018-07-18 11:32:45 +0800

    Register Activity added (Use of camera is integrated).

--- app/src/main/java/edu/admu/cs295s28/attendancechecker/MainActivity.java
+++ app/src/main/java/edu/admu/cs295s28/attendancechecker/MainActivity.java
@@ -3,6 +3,7 @@ package edu.admu.cs295s28.attendancechecker;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.TextView;
 
 import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
@@ -15,9 +16,18 @@ public class MainActivity extends AppCompatActivity {
     @ViewById(R.id.button)
     Button login;
 
+    @ViewById(R.id.button2)
+    Button register;
+
+    @Click(R.id.button2)
+    public void register(){
+        RegisterActivity_.intent(this).start();
+    }
+
+
     @Click(R.id.button)
     public void login(){
-        UserAccount_.intent(this).start();
+    UserAccount_.intent(this).start();
     }
 
     /*@ViewById(R.id.btnMaps)
--- app/src/main/java/edu/admu/cs295s28/attendancechecker/RegisterActivity.java
+++ app/src/main/java/edu/admu/cs295s28/attendancechecker/RegisterActivity.java
@@ -4,12 +4,16 @@ import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.Spinner;
 
 import com.squareup.picasso.MemoryPolicy;
 import com.squareup.picasso.NetworkPolicy;
 import com.squareup.picasso.Picasso;
 
+import org.androidannotations.annotations.AfterViews;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.ViewById;
@@ -21,6 +25,8 @@ import java.io.IOException;
 import io.realm.Realm;
 import io.realm.RealmResults;
 
+import static android.app.Activity.RESULT_CANCELED;
+
 @EActivity (R.layout.activity_register)
 public class RegisterActivity extends AppCompatActivity {
     Picasso picasso;
@@ -28,15 +34,42 @@ public class RegisterActivity extends AppCompatActivity {
     Intent intent;
     Realm realm;
 
-    @ViewById
+    @ViewById(R.id.imageView)
     ImageView imageView;
+
+    @ViewById(R.id.btnAdd)
+    Button btnAdd;
+
+    @ViewById(R.id.btnCancel)
+    Button btnCancel;
 
    /* @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+
     }*/
-   @Click(R.id.imageView)
+
+    @AfterViews
+    public void init() {
+        picasso = Picasso.get();
+        refreshImageView(R.mipmap.ic_launcher);
+    }
+
+    @Click(R.id.btnAdd)
+    public void add(){
+
+    }
+
+    @Click(R.id.btnCancel)
+    public void cancel() {
+        //close the screen back to the main screen
+        intent = new Intent();
+        setResult(RESULT_CANCELED, intent);
+        finish();
+    }
+
+    @Click(R.id.imageView)
    public void selectPic() {
        ImageActivity_.intent(this).startForResult(0);
    }
@@ -85,11 +118,11 @@ public class RegisterActivity extends AppCompatActivity {
 
         picasso.load(savedImage).networkPolicy(NetworkPolicy.NO_CACHE).memoryPolicy(MemoryPolicy.NO_CACHE).into(imageView);
     }
-    public void onDestroy() {
+/*    public void onDestroy() {
         super.onDestroy();
         if (!realm.isClosed()) {
             System.out.println("DESTROYED...");
             realm.close();
         }
-    }
+    }*/
 }
